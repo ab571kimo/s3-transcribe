74c503dfabf7d201cd0585990701993086a2ae40
CCYang <ab571kimo@gmail.com>
2024-10-24 17:46:02 +0800

    no message

--- src/main/java/controller/TranscribeController.java
+++ src/main/java/controller/TranscribeController.java
@@ -6,9 +6,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 import software.amazon.awssdk.services.transcribe.TranscribeClient;
-import software.amazon.awssdk.services.transcribe.model.LanguageCode;
-import software.amazon.awssdk.services.transcribe.model.Media;
-import software.amazon.awssdk.services.transcribe.model.StartTranscriptionJobRequest;
+import software.amazon.awssdk.services.transcribe.model.*;
+
 import java.util.UUID;
 
 public class TranscribeController implements RequestHandler<S3Event, String> {
@@ -16,7 +15,7 @@ public class TranscribeController implements RequestHandler<S3Event, String> {
     @Override
     public String handleRequest(S3Event event, Context context) {
 
-        S3EventNotificationRecord record = event.getRecords().get(0);
+        S3EventNotificationRecord record = event.getRecords().getFirst();
         String srcBucket = record.getS3().getBucket().getName();
         String srcKey = record.getS3().getObject().getUrlDecodedKey();
         String region = record.getAwsRegion();
@@ -33,12 +32,22 @@ public class TranscribeController implements RequestHandler<S3Event, String> {
 
         StartTranscriptionJobRequest request = StartTranscriptionJobRequest.builder()
                 .transcriptionJobName(jobName)
-                .mediaFormat("mp3")
+                .mediaFormat("mp4")
                 .languageCode(LanguageCode.ZH_TW)
+                .languageOptions(LanguageCode.ZH_TW, LanguageCode.EN_US)
                 .outputBucketName(srcBucket)
                 .outputKey(srcKey + ".json")
+                .settings(Settings.builder()
+                        .showSpeakerLabels(true)
+                        .maxSpeakerLabels(10)
+                        .channelIdentification(false)
+                        .showAlternatives(false)
+                        .build())
                 .media(Media.builder()
                         .mediaFileUri(fileUrl)
+                        .build())
+                .subtitles(Subtitles.builder()
+                        .formats(SubtitleFormat.SRT, SubtitleFormat.VTT)
                         .build())
                 .build();
 
